6ec8a4ec9a0d2c1088ed19a8a6941524d50eea10
ANDREA <andreahernandez.bos@gmail.com>
2017-03-15 17:49:59 -0700

    Assignment 9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-    
-    var revealPoint = function(index) {
+var revealPoint = function(index) {
         index.style.opacity = 1;
         index.style.transform = "scale(1,1) translateY(0)";
         index.style.msTransform = "scale(1,1) translateY(0)";
         index.WebkitTransform = "scale(1,1) translateY(0)";
     };
+
+var animatePoints = function(points) {
     
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(points[i]);
-    }
+    forEach(points, revealPoint);
                 
 };
 
